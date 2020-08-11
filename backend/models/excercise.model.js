0c4ecc3f0d05e2a4bd399d44ab27805e7a7dea7f
bhavyakedar <58164903+bhavyakedar@users.noreply.github.com>
2020-08-12 00:10:55 +0530

    First Commit

---
+++ backend/models/excercise.model.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const excerciseSchema = new Schema({
+    username : {type : String, required : true},
+    description : {type : String, required : true},
+    duration : {type : Number, required : true},
+    date : {type : Date, required : true}
+},
+{ timestamps : true });
+
+const Excercise = mongoose.model('Excersice',excerciseSchema);
+
+module.exports = Excercise
